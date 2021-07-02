5f25904d27d3a7027f62e76b29f31f0ff50d70d7
ekiauhce <ekiauhce@yandex.ru>
2021-07-02 16:26:47 +0300

    Create NavigationList.vue

---
+++ src/components/NavigationList.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-list nav dense>
+    <v-list-item to="/products" v-if="this.$store.getters.credentials">
+      <v-list-item-icon>
+        <v-icon>mdi-clipboard-text</v-icon>
+      </v-list-item-icon>
+      <v-list-item-content>
+        <v-list-item-title>Products</v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+
+    <v-list-item to="/login" v-if="!this.$store.getters.credentials">
+      <v-list-item-icon>
+        <v-icon>mdi-login</v-icon>
+      </v-list-item-icon>
+      <v-list-item-content>
+        <v-list-item-title>Login</v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+
+    <v-list-item to="/register" v-if="!this.$store.getters.credentials">
+      <v-list-item-icon>
+        <v-icon>mdi-account-plus</v-icon>
+      </v-list-item-icon>
+      <v-list-item-content>
+        <v-list-item-title>Register</v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+
+    <v-list-item @click="logout" v-if="this.$store.getters.credentials">
+      <v-list-item-icon>
+        <v-icon>mdi-logout</v-icon>
+      </v-list-item-icon>
+      <v-list-item-content>
+        <v-list-item-title>Logout</v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+  </v-list>
+</template>
+
+<script>
+export default {
+  name: "NavigationList",
+  methods: {
+    logout() {
+      this.$store.dispatch("logout")
+          .then(() => this.$router.push("/login"));
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
