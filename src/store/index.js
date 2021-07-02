a04ac0c80ef83f29775a217c140037f32fae191d
ekiauhce <ekiauhce@yandex.ru>
2021-07-02 16:24:41 +0300

    Add x-requested-with header some actions

--- src/store/index.js
+++ src/store/index.js
@@ -26,31 +26,51 @@ const mutations = {
 }
 
 const actions = {
-    setCredentials(context, payload) {
-        axios.post("/api/register", payload)
-            .then(response => {
-                context.commit("SET_CREDENTIALS", response.data);
+    register(context, payload) {
+        axios.post("/api/register", payload,
+            { headers: {"X-Requested-With": "XMLHttpRequest"} })
+            .then();
+    },
+    login(context, payload) {
+        axios.post("/api/login", payload,
+            { headers: {"X-Requested-With": "XMLHttpRequest"} })
+            .then(() => {
+                localStorage.setItem("credentials", JSON.stringify(payload));
+                context.commit("SET_CREDENTIALS", payload);
             });
     },
+    logout({ commit }) {
+        return new Promise(resolve => {
+            localStorage.removeItem("credentials");
+            commit("SET_CREDENTIALS", null);
+            resolve();
+        })
+    },
+    setCredentials(context, payload) {
+        context.commit("SET_CREDENTIALS", payload);
+    },
     getProductItems(context) {
-        axios.get("/api/products", { auth: context.getters.credentials })
+        axios.get("/api/products",
+            { auth: context.getters.credentials, headers: {"X-Requested-With": "XMLHttpRequest"} })
             .then(response => {
                 context.commit("UPDATE_PRODUCT_ITEMS", response.data)
             });
     },
     addNewProduct(context, payload) {
-        axios.post("/api/products", payload, { auth: context.getters.credentials })
+        axios.post("/api/products", payload,
+            { auth: context.getters.credentials, headers: {"X-Requested-With": "XMLHttpRequest"} })
             .then(response =>  {
                 context.commit("ADD_PRODUCT", response.data);
             });
     },
     addNewPurchase(context, { productId, purchase }) {
         axios.post(`/api/products/${productId}/purchases`,
-            purchase, { auth: context.getters.credentials })
+            purchase, { auth: context.getters.credentials, headers: {"X-Requested-With": "XMLHttpRequest"}})
             .then(response => {
                 context.commit("ADD_PURCHASE", { productId, purchase: response.data })
             });
-    }
+    },
+
 }
 
 const getters = {
