5099e6cd3ca48a246757eb21b57b7ebe19350a76
ekiauhce <ekiauhce@yandex.ru>
2021-07-14 16:07:35 +0300

    Change primary and secondary theme colors

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,9 +1,19 @@
 import Vue from "vue";
-import Vuetify from "vuetify";
-import "vuetify/dist/vuetify.min.css";
+import Vuetify from "vuetify/lib";
+
+import colors from "vuetify/lib/util/colors";
 
 Vue.use(Vuetify);
 
-const opts = {};
+const opts = {
+    theme: {
+        themes: {
+            light: {
+                primary: colors.blueGrey.darken1,
+                secondary: colors.teal.lighten3
+            }
+        }
+    }
+};
 
 export default new Vuetify(opts);
