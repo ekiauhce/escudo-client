4f10a646e14c74ca0b8e3b3852c476bafd6aa4bb
ekiauhce <ekiauhce@yandex.ru>
2021-08-14 21:16:49 +0300

    Refactoring

--- src/components/purchases/PurchaseList.vue
+++ src/components/purchases/PurchaseList.vue
@@ -1,17 +1,28 @@
 <template>
-  <v-container>
-    <v-row justify="center">
+  <v-container class="pa-0">
+    <v-row justify="center" no-gutters>
       <v-col sm="8" md="6" lg="4">
-        <v-container class="px-2">
-          <v-row v-if="productItem">
+        <v-container class="px-1" >
+          <v-row no-gutters class="mx-1" v-if="productItem">
+            <v-col cols="12">
+              <ProductTitle :productItem="productItem"/>
+            </v-col>
+          </v-row>
+          <v-row no-gutters class="my-3 mx-1">
+            <v-col cols="12">
+            <PurchasesSummary :productItem="productItem"/>
+            </v-col>
+          </v-row>
+          <v-row v-if="productItem" no-gutters>
             <PurchaseListItem v-for="purchaseItem in productItem.purchases" :key="purchaseItem.id"
-                              :purchaseItem="purchaseItem" :productItemId="productItem.id"/>
+              :purchaseItem="purchaseItem" :productItemId="productItem.id"/>
+          </v-row>
+          <v-row no-gutters class="my-3 mx-1">
+            <v-col>
+            <PurchaseEntry :productItem="productItem"/>
+            </v-col>
           </v-row>
         </v-container>
-        <v-divider class="mt-3"></v-divider>
-        <PurchasesSummary :productItem="productItem"/>
-        <v-divider></v-divider>
-        <PurchaseEntry :productItem="productItem"/>
       </v-col>
     </v-row>
   </v-container>
@@ -22,6 +33,7 @@
 import PurchaseListItem from "./PurchaseListItem";
 import PurchaseEntry from "./PurchaseEntry";
 import PurchasesSummary from "./PurchasesSummary";
+import ProductTitle from "../products/ProductTitle";
 import {mapGetters} from "vuex";
 
 export default {
@@ -29,7 +41,8 @@ export default {
   components: {
     PurchasesSummary,
     PurchaseEntry,
-    PurchaseListItem
+    PurchaseListItem,
+    ProductTitle
   },
   props: ["productName"],
   computed: {
