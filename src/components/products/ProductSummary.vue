55cfd8cff1b9670428975e65ee0618df5e6573d0
ekiauhce <ekiauhce@yandex.ru>
2021-08-20 19:17:47 +0300

    Create product summary component

---
+++ src/components/products/ProductSummary.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-sheet outlined rounded>
+    <v-container>
+      <v-row dense>
+        <v-col>
+          Average <abbr title="Cost per day">CPD</abbr>:
+        </v-col>
+        <v-col class="text-right">
+          <strong>{{ productItem.summary.avgCPD.toFixed(2) }}</strong> <small>₽/day</small>
+        </v-col>
+      </v-row>
+      <v-row dense>
+        <v-col>
+          Average lifespan:
+        </v-col>
+        <v-col class="text-right">
+          <strong>{{ productItem.summary.avgLifespan.toFixed(2) }}</strong> <small>days</small>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-sheet>
+</template>
+
+<script>
+export default {
+  name: "ProductSummary",
+  props: ["productItem"]
+}
+</script>
+
+<style scoped>
+
+</style>
