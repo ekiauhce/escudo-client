1d36cf8335a6b28b881550897bc2f93cba6ad917
ekiauhce <ekiauhce@yandex.ru>
2021-08-20 19:27:22 +0300

    add todo

--- src/components/products/ProductListItem.vue
+++ src/components/products/ProductListItem.vue
@@ -7,55 +7,26 @@
               {{ product.name }}
             </v-col>
             <v-col class="text-right">
-              <strong v-if="lastPurchase">{{ lastPurchaseCostPerDay }}</strong>
+              <!-- TODO: add some info about product on the right of list item -->
+              <!-- <strong v-if="lastPurchase">{{ lastPurchaseCostPerDay }}</strong>
               <strong v-else>0</strong>
-              <small> ₽/day</small>
+              <small> ₽/day</small> -->
             </v-col>
           </v-row>
         </v-list-item-title>
-        <v-list-item-subtitle v-if="lastPurchase">
-          {{ lastPurchaseMadeAtMomentFormatted }}
-        </v-list-item-subtitle>
+        <!-- <v-list-item-subtitle>
+          sdsds
+        </v-list-item-subtitle> -->
       </v-list-item-content>
     </v-list-item>
 </template>
 
 <script>
-import {mapGetters} from "vuex";
-import moment, {duration} from "moment";
-
 export default {
   name: "ProductListItem",
   props: ["product"],
   data() {
     return {
-      lastPurchase: null,
-      lastPurchaseMadeAtMoment: null,
-      now: moment()
-    }
-  },
-  created() {
-    this.lastPurchase = this.lastPurchaseByProductId(this.product.id);
-    if (this.lastPurchase) {
-      this.lastPurchaseMadeAtMoment = moment(this.lastPurchase.madeAt);
-    }
-  },
-  mounted() {
-    setInterval(() => this.now = moment(), 1000);
-  },
-  computed: {
-    ...mapGetters([
-        "lastPurchaseByProductId"
-    ]),
-    lastPurchaseMadeAtMomentFormatted() {
-      return this.lastPurchaseMadeAtMoment.from(this.now);
-    },
-    lastPurchaseCostPerDay() {
-      const price = this.lastPurchase.price;
-      const days = duration(this.now.diff(this.lastPurchaseMadeAtMoment))
-          .asSeconds() / (60 * 60 * 24);
-
-      return (price / days).toFixed(2);
     }
   }
 }
