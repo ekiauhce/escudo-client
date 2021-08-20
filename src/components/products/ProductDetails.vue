0535bc4c8aa256a890855259324a9316e5e86e06
ekiauhce <ekiauhce@yandex.ru>
2021-08-20 19:16:47 +0300

    Refactoring: create product details component

---
+++ src/components/products/ProductDetails.vue
@@ -0,0 +1,84 @@
+<template>
+    <v-container fluid>
+      <v-row no-gutters justify="center">
+      <v-col sm="8" md="6" lg="4">
+        <v-row>
+          <v-col cols="12">
+            <ProductTitle :productItemName="productName" v-if="productItem"/>
+            <v-skeleton-loader v-else type="card-title"></v-skeleton-loader>
+          </v-col>
+          
+          <v-col cols="12">
+            <div v-if="productItem">
+              <ProductSummary :productItem="productItem"/>
+            </div>
+            <div v-else>
+              <v-skeleton-loader type="list-item-two-line"></v-skeleton-loader>
+            </div>
+          </v-col>
+
+          <v-col cols="12">
+            <PurchaseList :productItem="productItem"/>
+          </v-col>
+
+          <v-col cols="12">
+            <div v-if="productItem">
+              <PurchaseEntry :productItem="productItem"/>
+            </div>
+            <div v-else>
+              <v-skeleton-loader type="list-item@2"></v-skeleton-loader>
+            </div>
+          </v-col>
+        </v-row>
+      </v-col>
+      </v-row>
+  </v-container>
+</template>
+
+<script>
+
+import PurchaseEntry from "../purchases/PurchaseEntry";
+import ProductSummary from "./ProductSummary";
+import ProductTitle from "./ProductTitle";
+import {mapGetters} from "vuex";
+import PurchaseList from '../purchases/PurchaseList';
+
+export default {
+  name: "ProductDetails",
+  components: {
+    ProductSummary,
+    PurchaseEntry,
+    PurchaseList,
+    ProductTitle
+  },
+  props: ["productName"],
+  data() {
+    return {
+      productItem: null
+    }
+  },
+  created() {
+    if (this.productItems == null) {
+      this.$store.dispatch("getProductItems")
+        .then(() => {
+          this.productItem = this.$store.getters.productItemFromName(this.productName);
+        })
+        .then(() => this.$store.dispatch("getPurchases", { productName: this.productName }))
+        .catch(() => this.$router.replace("/not-found"));
+    } else if (this.productItem == null) {
+      this.productItem = this.$store.getters.productItemFromName(this.productName);
+      if (this.productItem.purchases == null) {
+        this.$store.dispatch("getPurchases", { productName: this.productName });
+      }
+    }
+  },
+  computed: {
+    ...mapGetters(["productItems"])
+  }
+}
+</script>
+
+<style scoped>
+
+
+</style>
--- src/components/purchases/PurchaseList.vue
+++ src/components/purchases/PurchaseList.vue
@@ -1,26 +1,14 @@
 <template>
-    <v-container fluid>
-      <v-row no-gutters justify="center">
-      <v-col sm="8" md="6" lg="4" v-if="productItem">
-        <v-row>
-          <v-col cols="12">
-            <ProductTitle :productItem="productItem"/>
-          </v-col>
-          <v-col cols="12">
-            <PurchasesSummary :productItem="productItem"/>
-          </v-col>
-          <v-col cols="12">
-            <v-row dense>
-              <v-col cols="6" v-for="purchaseItem in productItem.purchases" :key="purchaseItem.id">
-                <PurchaseListItem :purchaseItem="purchaseItem" :productItemId="productItem.id"/>
-              </v-col>
-            </v-row>
-          </v-col>
-          <v-col cols="12">
-          <PurchaseEntry :productItem="productItem"/>
-          </v-col>
-        </v-row>
-      </v-col>
+    <v-container fluid class="pa-0">
+      <v-row dense v-if="productItem && productItem.purchases != null">
+        <v-col cols="6" v-for="purchaseItem in productItem.purchases" :key="purchaseItem.id">
+          <PurchaseListItem :purchaseItem="purchaseItem" :productItemName="productItem.name"/>
+        </v-col>
+      </v-row>
+      <v-row dense v-else>
+        <v-col cols="6" v-for="index in 4" :key="index">
+          <v-skeleton-loader type="list-item-two-line, actions"></v-skeleton-loader>
+        </v-col>
       </v-row>
   </v-container>
 </template>
@@ -28,35 +16,13 @@
 <script>
 
 import PurchaseListItem from "./PurchaseListItem";
-import PurchaseEntry from "./PurchaseEntry";
-import PurchasesSummary from "./PurchasesSummary";
-import ProductTitle from "../products/ProductTitle";
-import {mapGetters} from "vuex";
 
 export default {
   name: "PurchaseList",
-  components: {
-    PurchasesSummary,
-    PurchaseEntry,
-    PurchaseListItem,
-    ProductTitle
-  },
-  props: ["productName"],
-  computed: {
-    ...mapGetters(["isProductItemsEmpty"]),
-    productItem() {
-      return this.$store.getters.productItemFromName(this.productName);
-    }
-  },
-  watch: {
-    isProductItemsEmpty() {
-      if (!this.productItem) this.$router.push("/not-found");
-    }
-  }
+  components: { PurchaseListItem },
+  props: ["productItem"]
 }
 </script>
 
 <style scoped>
-
-
 </style>
